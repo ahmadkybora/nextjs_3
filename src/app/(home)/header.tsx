7ec24ccc1443c0333c389b3bc6928db829f12d6d
ahmadkybora <ahmadkybora@gmail.com>
2023-09-09 15:33:08 +0330

    make Feature-Avatar and use to header

--- src/app/(home)/header.tsx
+++ src/app/(home)/header.tsx
@@ -2,17 +2,22 @@
 
 import { Container } from "@/components";
 import headerStyles from "./home.module.scss";
+import { Avatar } from "@/components";
+import avatarImage from "/public/images/profile/avatar.png";
 
 const Header = () => {
     interface Styles {
-        containerLogoHamburgerMenu?: string;
+        containerLogoAvatar?: string;
         header?: string;
     }
-    const { containerLogoHamburgerMenu, header }: Styles = headerStyles;
+    const { containerLogoAvatar, header }: Styles = headerStyles;
     return(
         <>
             <Container style={header}>
-                <Container style={containerLogoHamburgerMenu}>
+                <Container style={containerLogoAvatar}>
+                </Container>
+                <Container style={containerLogoAvatar}>
+                    <Avatar src={avatarImage.src} />
                 </Container>
             </Container>
         </>
